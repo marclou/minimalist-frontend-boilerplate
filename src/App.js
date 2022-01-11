5e01326fad16555e546a8fc4fbfe80feac4c3d08
Marc Lou <marc.louvion@gmail.com>
2022-01-11 17:03:13 +0800

    adding daisyUI library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import Blank from './';
-
 function App() {
-  return <>Hey you</>;
+  return (
+    <>
+      <button class="btn btn-primary">DaisyUI Button</button>
+      <ul class="steps steps-horizontal">
+        <li class="step step-primary">Register</li>
+        <li class="step step-primary">Choose plan</li>
+        <li class="step">Purchase</li>
+        <li class="step">Receive Product</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
