67da1915ce90d5093cd49939c5a0d0a439bd3491
Marc Lou <marc.louvion@gmail.com>
2022-01-11 22:19:02 +0800

    adding basic routes & 404 page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Loader from './pages/Loader';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route index element={<div>home bro</div>} />
+          <Route path="/" element={<div>home bro</div>} />
 
           <Route path="login" element={<div>LOGIN</div>} />
 
-          <Route path="*" element={<div>HOME</div>} />
+          <Route path="register" element={<div>REGISTER</div>} />
+
+          <Route path="dashboard" element={<div>Dashboard</div>}></Route>
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
--- src/pages/Loader.js
+++ src/pages/Loader.js
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="w-full h-full fixed flex top-0 left-0 items-center justify-center">
+    <div className="w-screen h-screen fixed flex top-0 left-0 items-center justify-center">
       <svg width={38} height={38} xmlns="http://www.w3.org/2000/svg" className="stroke-black dark:stroke-white">
         <g transform="translate(1 1)" strokeWidth={2} fill="none" fillRule="evenodd">
           <circle strokeOpacity={0.5} cx={18} cy={18} r={18} />
