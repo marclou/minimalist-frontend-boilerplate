a9c8bedb9de8e8bb08c99e43d164bcb7a9e86b6c
Marc Lou <marc.louvion@gmail.com>
2022-01-13 15:00:21 +0800

    match ui for toasts & daisyui library

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { useContext } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { ThemeContext } from '../contexts/theme';
 import Header from './Header';
 import ErrorBoundary from '../pages/Error';
 import Loader from '../pages/Loader';
 
 const Layout = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <div className="min-h-screen w-screen p-2">
-      <ToastContainer />
+      <ToastContainer theme={theme} />
       <Header />
       <ErrorBoundary>
         <Suspense fallback={<Loader />}>
