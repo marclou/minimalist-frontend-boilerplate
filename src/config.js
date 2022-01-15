874f6608716eb023fde02e87ea6e815a6d739077
Marc Lou <marc.louvion@gmail.com>
2022-01-15 15:50:58 +0800

    adding .env var

--- src/config.js
+++ src/config.js
@@ -1,6 +1,21 @@
+const Joi = require('joi');
+
+const envVarsSchema = Joi.object()
+  .keys({
+    REACT_APP_API_URL: Joi.string().required().description('URL for the API'),
+    REACT_APP_EMAIL_SUPPORT_TO: Joi.string().required().description('The email app users can reach out for support'),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
 const config = {
-  apiUrl: process.env.NODE_ENV === 'production' ? 'https://magic-habit-api.herokuapp.com/v1/' : 'http://localhost:8080/v1/',
-  emailSupport: 'marc.louvion@gmail.com',
+  apiUrl: envVars.REACT_APP_API_URL,
+  emailSupport: envVars.REACT_APP_EMAIL_SUPPORT_TO,
 };
 
 export default config;
